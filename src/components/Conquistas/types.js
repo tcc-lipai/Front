15663afa72cf16c9181b7acd5196cda24277dc46
LipAI <leituralabialtcc@gmail.com>
2026-06-10 21:41:23 -0300

    Delete src/components directory

--- src/components/Conquistas/types.js
+++
@@ -1,4 +0,0 @@
-export const DEFAULT_CONQUISTA_DATA = {
-  title: 'Semana Ouro',
-  subtitle: 'Semana Ouro'
-};
--- src/components/LicaoConcluida/types.js
+++
@@ -1,5 +0,0 @@
-export const INITIAL_LESSON_STATS = {
-    percentage: 0,
-    stars: 0,
-    time: '00:00'
-  };
