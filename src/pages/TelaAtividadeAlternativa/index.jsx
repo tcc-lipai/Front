0d5946d29b315b07959d7238c75e80030a74d564
evellynfurtad0 <evellynfurtado100@gmail.com>
2026-06-18 23:03:18 -0300

    tirando o codigo errado

--- src/pages/TelaAtividadeAlternativa/index.jsx
+++ src/pages/TelaAtividadeAlternativa/index.jsx
@@ -1,67 +1,11 @@
-import React from 'react';
-import './index.css';
-import Botao from '../../components/Botao';
-import { X } from 'lucide-react';
+// Parte do Feedback
+// import React from 'react';
+// import FeedbackCard from '../../components/FeedbackCard';
+// import { useFeedback } from '../../components/FeedbackCard/hooks';
+// import { FEEDBACK_TYPES } from '../../components/FeedbackCard/types';
 
 const TelaAtividadeAlternativa = () => {
-  return (
-    <div className="atividade-overlay">
-      <div className="atividade-container">
-
-        <div className="atividade-header">
-          <div className="atividade-titulo">
-            <h1>Primeira Atividade</h1>
-            <span>Unidade 1</span>
-          </div>
-
-          <div className="atividade-progresso">
-            <div className="barra-progresso">
-              <div className="progresso"></div>
-            </div>
-          </div>
-
-          <button className="btn-fechar">
-            <X size={38} />
-          </button>
-        </div>
-
-        {/* Aqui depois entra o vídeo vindo do back */}
-        <div className="video-container">
-          <div className="video-placeholder">
-            <span>VIDEO AULA</span>
-          </div>
-        </div>
-
-        <div className="atividade-conteudo">
-          <span className="video-numero">Vídeo 1</span>
-
-          <h2>Título do vídeo</h2>
-
-          <p>
-            Aqui vai a descrição da vídeo aula e um mini resumo do que será
-            ensinado e sua importância lorem fesfes fafw frfs f e3afa fwaf
-            fffffffff adwad f wa fwafo hdhwhdw
-          </p>
-
-          <p>
-            Aqui vai a descrição da vídeo aula e um mini resumo do que será
-            ensinado e sua importância lorem fesfes fafw frfs f e3afa fwaf
-            fffffffff adwad f wa fwafo hdhwhdw
-          </p>
-        </div>
-
-        <div className="atividade-botao">
-          <Botao
-            texto="Finalizar"
-            corDeFundo="#9065A6"
-            corTexto="#FFFFFF"
-            onClick={() => console.log('Finalizar')}
-          />
-        </div>
-
-      </div>
-    </div>
-  );
+  
 };
 
 export default TelaAtividadeAlternativa;
